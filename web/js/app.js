a317cb4e08b827c406355a7100042eace730d5e2
StudioKode <julia@studiokode.no>
2021-04-29 12:56:51 +0200

    Removed Glider from app.js

--- web/js/app.js
+++ web/js/app.js
@@ -1,15 +1,1 @@
-// GLIDER
-new Glider(document.querySelector('.glider'), {
-    slidesToShow: 5,
-    slidesToScroll: 5,
-    draggable: true,
-    dots: '.dots',
-    arrows: {
-      prev: '.glider-prev',
-      next: '.glider-next'
-    }
-  });
-
-  console.log("test");
-  var test = "Dette er en test";
- var hello = "Hello World";
+console.log("File loaded");
