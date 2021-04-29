3e75c6713dd68f30568802d9defb114da270516a
StudioKode <julia@studiokode.no>
2021-04-29 10:37:14 +0200

    Added app.js file.

---
+++ web/js/app.js
@@ -0,0 +1,15 @@
+// GLIDER
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    draggable: true,
+    dots: '.dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+  });
+
+  console.log("test");
+  var test = "Dette er en test";
+ 
