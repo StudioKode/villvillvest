06362bcd0c7937de215809263f1a5fe15703de46
StudioKode <julia@studiokode.no>
2021-05-20 15:59:58 +0200

    Removed capital letters in nav and footer.

--- web/js/script.js
+++ web/js/script.js
@@ -75,3 +75,41 @@ burgers.forEach(burger => burger.addEventListener('click', () => {
 
 console.log("loaded");
 console.log(nav.style.opacity);
+
+
+//ACCORDION
+
+const accordion = document.querySelector('.accordion');
+const items = accordion.querySelectorAll('.accordion__item');
+
+items.forEach((item) => {
+  const title = item.querySelector('.accordion__title');
+  
+  title.addEventListener('click', (e) => {
+    const opened_item = accordion.querySelector('.is-open');
+    
+    // Toggle current item
+    toggle_item(item); 
+    
+    // Close earlier opened item if exists
+    if (opened_item && opened_item !== item) {
+      toggle_item(opened_item);
+    }
+    
+    
+  });
+});
+
+const toggle_item = (item) => {
+  const body = item.querySelector('.accordion__body');
+  const content = item.querySelector('.accordion__content');
+        
+  if (item.classList.contains('is-open')) {
+    body.removeAttribute('style');
+    item.classList.remove('is-open');
+  }else {
+    body.style.height = body.scrollHeight + 'px';
+    item.classList.add('is-open');
+  }
+}
+  
